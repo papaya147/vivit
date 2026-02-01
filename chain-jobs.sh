6f3700cae0bfa9785369a6e85f94a59f58debbe1
Abhinav Srivatsa <asrivatsa6@gmail.com>
2026-01-31 18:52:56 -0800

    updated carc scripts

--- chain-jobs.sh
+++ chain-jobs.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Name of your slurm script
 JOB_SCRIPT="train-carc.slurm"
-
-# How many jobs to chain
-NUM_JOBS=5
+NUM_JOBS=10
 
 echo "Submitting Job 1..."
+# Initial job submission
 JOB_ID=$(sbatch --parsable $JOB_SCRIPT)
 echo "Job 1 ID: $JOB_ID"
 
 for i in $(seq 2 $NUM_JOBS); do
     echo "Submitting Job $i (dependent on $JOB_ID)..."
-    JOB_ID=$(sbatch --parsable --dependency=aftercorr:$JOB_ID $JOB_SCRIPT)
+    JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID $JOB_SCRIPT)
     echo "Job $i ID: $JOB_ID"
 done
